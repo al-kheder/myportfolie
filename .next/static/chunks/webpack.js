9fe7c5a2ef4146f690631035b7c995f14a465d68
ali al-kheder <alkhdyyr@gmail.com>
2025-01-17 18:45:46 +0100

    fix the import issur in the main component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec86760eea16e3d4"; }
+/******/ 		__webpack_require__.h = function() { return "e535c5996e1757f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
